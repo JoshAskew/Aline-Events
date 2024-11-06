8c19c0de9aa817d58170f4d6fea1fa21108d2348
JoshAskew <asksprit3@gmail.com>
2024-11-06 10:15:46 -0600

    changed landing page

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,25 +1,48 @@
+import { Box, Heading, Text, Button, VStack, HStack, Icon } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import { Button } from "../components/ui/button"
-import { RiArrowRightLine } from "react-icons/ri"
+import { RiArrowRightLine } from "react-icons/ri";
 
-const Home = () => {
+const LandingPage = () => {
     return (
-        <div className="home-container">
-        <header className="home-header">
-          <h1>Welcome to Aline</h1>
-          <p>Discover events, explore, and never miss out on what's happening around you!</p>
-        </header>
-  
-        <div>
-          <Link to="/login">
-          <Button size="xl" variant="ghost">Login <RiArrowRightLine /></Button>
-          </Link>
-          <Link to="/signup">
-          <Button size="xl" variant="ghost">Sign Up</Button>
-          </Link>
-        </div>
-      </div>
+        <Box 
+            display="flex" 
+            flexDirection="column" 
+            alignItems="center" 
+            justifyContent="center" 
+            minHeight="100vh" 
+            bgGradient="linear(to-r, gray.50, teal.100)"
+            p={4}
+        >
+            <VStack textAlign="center">
+                <Heading as="h1" size="2xl" color="teal.600">
+                    Welcome to Aline
+                </Heading>
+                <Text fontSize="xl" color="gray.600" maxWidth="600px">
+                    Discover events, explore, and never miss out on what's happening around you!
+                </Text>
+                <HStack  mt={4}>
+                    <Link to="/login">
+                        <Button 
+                            size="lg" 
+                            colorScheme="teal" 
+                            variant="solid" 
+                        >
+                            Login <RiArrowRightLine />
+                        </Button>
+                    </Link>
+                    <Link to="/signup">
+                        <Button 
+                            size="lg" 
+                            colorScheme="teal" 
+                            variant="outline"
+                        >
+                            Sign Up
+                        </Button>
+                    </Link>
+                </HStack>
+            </VStack>
+        </Box>
     );
 };
 
-export default Home;
+export default LandingPage;
