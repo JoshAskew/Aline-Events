57de952e4af15c09d3292510d0c3ae7754c93562
cgroth06 <christopher.m.groth@gmail.com>
2024-11-06 12:51:10 -0600

    client login.tsx page state and event

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -4,11 +4,38 @@ import { Field } from "../components/ui/field"
 import { Link } from 'react-router-dom'
 import "./Login.css"
 
+import { useState, ChangeEvent, FormEvent } from 'react';
+import { login } from '../api/authAPI';
+import Auth from '../utils/auth';
+
 const Login = () => {
+  const [loginData, setLoginData] = useState({
+    username: '',
+    password: '',
+    zipCode: ''
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    setLoginData({
+      ...loginData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      const data = await login(loginData);
+      Auth.login(data.token);
+    } catch (err) {
+      console.error('Failed to login', err);
+    }
+  };
     return (
       <>
       <div className="form-container">
-      <form>
+      <form onSubmit={handleSubmit}>
       <Fieldset.Root size="lg" maxW="md">
     <Stack>
       <Fieldset.Legend>Login</Fieldset.Legend>
@@ -16,11 +43,14 @@ const Login = () => {
 
     <Fieldset.Content>
       <Field label="User Name">
-        <Input name="User Name" />
       </Field>
 
+      <Field label="Zip Code">
+        <Input name="zipCode" onChange={handleChange} />
+      </Field>
+      
       <Field label="Password">
-        <Input name="password" type="password" />
+        <Input name="password" type="password" onChange={handleChange} />
       </Field>
     
     </Fieldset.Content>
