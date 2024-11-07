41b8e4d2ca05b2b3096bf1cacef346cd4988e003
Avery Jacobson <averyajacobson@gmail.com>
2024-11-06 22:10:03 -0600

    updated home return to have both returns and we can decide which one we want to use

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -32,17 +32,17 @@ const Home: React.FC = () => {
 
                 const detailedEvents = await Promise.all(
                     allEvents.map(async (_allEvents) => {
-                        // Fetch detailed data for each candidate by username
-                        //const detailedData = await searchGithubUser(allCandidates.login);
+                      // Fetch detailed data for each candidate by username
+                      //const detailedData = await searchGithubUser(allCandidates.login);
 
-                        //   return detailedData;
+                   //   return detailedData;
 
-                        return [][0];
+                   return [][0];
                     })
-                );
-
-                // Set candidates with detailed data
-                setEvents(detailedEvents);
+                  );
+          
+                  // Set candidates with detailed data
+                  setEvents(detailedEvents);
 
             } catch (err) {
                 console.error('Failed to fetch events:', err);
@@ -91,7 +91,7 @@ const Home: React.FC = () => {
             </Link>
             <PopoverRoot>
                 <PopoverTrigger asChild>
-                    <Button className="logout" size="sm" variant="outline">
+                    <Button className="logout" size="sm" variant="outline" >
                         Logout
                     </Button>
                 </PopoverTrigger>
@@ -112,22 +112,190 @@ const Home: React.FC = () => {
             </PopoverRoot>
             <h1 className="header">Aline</h1>
             <div className="cards-container">
-                <div>
-                    <div className="event-list">
-                        {(events.length > 0 && events.length - 1 !== eventsIndex) ? (
-                            <EventCard
-                                event={events[eventsIndex]}
-                                addToEventList={addToEventList}
-                                removeFromEvents={removeFromEvents}
-                            />
-                        ) : (
-                            <p>No Events found.</p>
-                        )}
-                    </div>
-                </div>
+                <Card.Root className="card" maxW="sm" overflow="hidden">
+                    <Image
+                        src={Aline}
+                    />
+                    <Card.Body gap="2">
+                        <Card.Title>Event Title</Card.Title>
+                        <Card.Description>
+                            This is the type of the event.
+                        </Card.Description>
+                        <Card.Description>
+                            Start Date-End Date
+                        </Card.Description>
+                        <Text textStyle="2xl" fontWeight="medium" letterSpacing="tight" mt="2">
+                            $450-$1000
+                        </Text>
+                    </Card.Body>
+                    <Card.Footer gap="2">
+                        <Button variant="solid">Save Event</Button>
+                        <Button variant="ghost">Skip Event</Button>
+                    </Card.Footer>
+                </Card.Root>
+
+                <Card.Root className="card" maxW="sm" overflow="hidden">
+                    <Image
+                        src={Aline}
+                    />
+                    <Card.Body gap="2">
+                        <Card.Title>Event Title</Card.Title>
+                        <Card.Description>
+                            This is the type of the event.
+                        </Card.Description>
+                        <Card.Description>
+                            Start Date-End Date
+                        </Card.Description>
+                        <Text textStyle="2xl" fontWeight="medium" letterSpacing="tight" mt="2">
+                            $450-$1000
+                        </Text>
+                    </Card.Body>
+                    <Card.Footer gap="2">
+                        <Button variant="solid">Save Event</Button>
+                        <Button variant="ghost">Skip Event</Button>
+                    </Card.Footer>
+                </Card.Root>
+
+                <Card.Root className="card" maxW="sm" overflow="hidden">
+                    <Image
+                        src={Aline}
+                    />
+                    <Card.Body gap="2">
+                        <Card.Title>Event Title</Card.Title>
+                        <Card.Description>
+                            This is the type of the event.
+                        </Card.Description>
+                        <Card.Description>
+                            Start Date-End Date
+                        </Card.Description>
+                        <Text textStyle="2xl" fontWeight="medium" letterSpacing="tight" mt="2">
+                            $450-$1000
+                        </Text>
+                    </Card.Body>
+                    <Card.Footer gap="2">
+                        <Button variant="solid">Save Event</Button>
+                        <Button variant="ghost">Skip Event</Button>
+                    </Card.Footer>
+                </Card.Root>
+
+                <Card.Root className="card" maxW="sm" overflow="hidden">
+                    <Image
+                        src={Aline}
+                    />
+                    <Card.Body gap="2">
+                        <Card.Title>Event Title</Card.Title>
+                        <Card.Description>
+                            This is the type of the event.
+                        </Card.Description>
+                        <Card.Description>
+                            Start Date-End Date
+                        </Card.Description>
+                        <Text textStyle="2xl" fontWeight="medium" letterSpacing="tight" mt="2">
+                            $450-$1000
+                        </Text>
+                    </Card.Body>
+                    <Card.Footer gap="2">
+                        <Button variant="solid">Save Event</Button>
+                        <Button variant="ghost">Skip Event</Button>
+                    </Card.Footer>
+                </Card.Root>
+
+                <Card.Root className="card" maxW="sm" overflow="hidden">
+                    <Image
+                        src={Aline}
+                    />
+                    <Card.Body gap="2">
+                        <Card.Title>Event Title</Card.Title>
+                        <Card.Description>
+                            This is the type of the event.
+                        </Card.Description>
+                        <Card.Description>
+                            Start Date-End Date
+                        </Card.Description>
+                        <Text textStyle="2xl" fontWeight="medium" letterSpacing="tight" mt="2">
+                            $450-$1000
+                        </Text>
+                    </Card.Body>
+                    <Card.Footer gap="2">
+                        <Button variant="solid">Save Event</Button>
+                        <Button variant="ghost">Skip Event</Button>
+                    </Card.Footer>
+                </Card.Root>
+
+                <Card.Root className="card" maxW="sm" overflow="hidden">
+                    <Image
+                        src={Aline}
+                    />
+                    <Card.Body gap="2">
+                        <Card.Title>Event Title</Card.Title>
+                        <Card.Description>
+                            This is the type of the event.
+                        </Card.Description>
+                        <Card.Description>
+                            Start Date-End Date
+                        </Card.Description>
+                        <Text textStyle="2xl" fontWeight="medium" letterSpacing="tight" mt="2">
+                            $450-$1000
+                        </Text>
+                    </Card.Body>
+                    <Card.Footer gap="2">
+                        <Button variant="solid">Save Event</Button>
+                        <Button variant="ghost">Skip Event</Button>
+                    </Card.Footer>
+                </Card.Root>
             </div>
         </>
     );
+
+    // Saying the same thing 
+
+    // return (
+    //     <>
+    //         <WeatherSidebar />
+    //         <Link to="/SavedEvents">
+    //             <Button className="saved-button" size="sm" variant="outline">Saved Events</Button>
+    //         </Link>
+    //         <PopoverRoot>
+    //             <PopoverTrigger asChild>
+    //                 <Button className="logout" size="sm" variant="outline">
+    //                     Logout
+    //                 </Button>
+    //             </PopoverTrigger>
+    //             <PopoverContent>
+    //                 <PopoverArrow />
+    //                 <PopoverBody>
+    //                     <PopoverTitle fontWeight="bold">Are you sure you want to logout?</PopoverTitle>
+    //                     <Text my="4">
+    //                         These prices are not guaranteed to persist.
+    //                     </Text>
+    //                     <Link to="../Login">
+    //                         <Button className="logout" size="sm" variant="outline">
+    //                             Yes, Log Me Out
+    //                         </Button>
+    //                     </Link>
+    //                 </PopoverBody>
+    //             </PopoverContent>
+    //         </PopoverRoot>
+    //         <h1 className="header">Aline</h1>
+    //         <div className="cards-container">
+    //             <div>
+    //                 <div className="event-list">
+    //                     {(events.length > 0 && events.length - 1 !== eventsIndex) ? (
+    //                         <EventCard
+    //                             event={events[eventsIndex]}
+    //                             addToEventList={addToEventList}
+    //                             removeFromEvents={removeFromEvents}
+    //                         />
+    //                     ) : (
+    //                         <p>No Events found.</p>
+    //                     )}
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     </>
+    // );
+
+
 };
 
 export default Home;
