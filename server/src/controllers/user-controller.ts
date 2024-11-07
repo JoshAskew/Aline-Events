dc501c369af4d57ccba9c152d2c99d760c9e1d0e
ChristopherP-C <christopherp.c.2day@gmail.com>
2024-11-07 08:54:00 -0600

    cleaned up various files

--- server/src/controllers/user-controller.ts
+++ server/src/controllers/user-controller.ts
@@ -1,17 +1,10 @@
 import { Request, Response } from 'express';
 import { User } from '../models/user.js';
 import bcrypt from 'bcrypt';
-import pkg from 'pg';
+import dotenv from 'dotenv';
 
-const { Pool } = pkg;
+dotenv.config();
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT || '5432', 10),
-});
 
 // GET /Users
 export const getAllUsers = async (_req: Request, res: Response) => {
@@ -46,36 +39,57 @@ export const getUserById = async (req: Request, res: Response) => {
 export const createUser = async (req: Request, res: Response) => {
   const { userName, password, zipCode } = req.body;
   try {
-
     if (!userName || !zipCode || !password) {
       return res.status(400).json({ message: 'All fields are required' });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const result = await pool.query(
-      `INSERT INTO users (userName, zipCode, password) 
-       VALUES ($1, $2, $3) 
-       RETURNING id`,
-      [userName, zipCode, hashedPassword]
-    );
+    const newUser = await User.create({
+      userName,
+      password: hashedPassword,
+      zipCode
+    });
 
-    return res.status(201).json({ message: 'User created successfully', userId: result.rows[0].id });
-    
+    return res.status(201).json({ message: 'User created successfully', userId: newUser.id });
   } catch (error: any) {
-    console.error(error);
-    return res.status(500).json({ message: 'Server error' });
+    return res.status(500).json({ message: error.message });
   }
 };
+
+// export const createUser = async (req: Request, res: Response) => {
+//   const { userName, password, zipCode } = req.body;
+//   try {
+    
+//     if (!userName || !zipCode || !password) {
+//       return res.status(400).json({ message: 'All fields are required' });
+//     }
+    
+//     const hashedPassword = await bcrypt.hash(password, 10);
+    
+//     const result = await pool.query(
+//       `INSERT INTO users (userName, zipCode, password) 
+//       VALUES ($1, $2, $3) 
+//       RETURNING id`,
+//       [userName, zipCode, hashedPassword]
+//     );
+    
+//     return res.status(201).json({ message: 'User created successfully', userId: result.rows[0].id });
+    
+//   } catch (error: any) {
+//     console.error(error);
+//     return res.status(500).json({ message: 'Server error' });
+//   }
+// };
 
 // PUT /Users/:id
 export const updateUser = async (req: Request, res: Response) => {
   const { id } = req.params;
-  const { username, password, zipCode } = req.body;
+  const { userName, password, zipCode } = req.body;
   try {
     const user = await User.findByPk(id);
     if (user) {
-      user.userName = username;
+      user.userName = userName;
       user.password = password;
       user.zipCode = zipCode;
       await user.save();
@@ -103,3 +117,15 @@ export const deleteUser = async (req: Request, res: Response) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// import pkg from 'pg';
+
+// const { Pool } = pkg;
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: parseInt(process.env.DB_PORT || '5432', 10),
+// });
