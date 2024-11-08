187663aa6242e6da707a88473ace954f81f822e6
DimintriLo <Dimintri.lo@gmail.com>
2024-11-08 11:26:58 -0600

    finshing touches to the about aline css

--- client/src/pages/AboutAline.tsx
+++ client/src/pages/AboutAline.tsx
@@ -5,22 +5,24 @@ import { useNavigate } from 'react-router-dom';
 
 const AboutAline = () => {
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleBack = () => {
-        navigate(-1);  
-    };
+  const handleBack = () => {
+  navigate(-1);  
+  };
 
-    return (
-      <section className="about-aline">
-        <button className="back-button2" onClick={handleBack}>Back</button>
-        <h1 className="header">About Aline</h1>
-        <p className="intro">
+  return (
+
+  <section className="about-aline">
+    <button className="back-button2" onClick={handleBack}>Back</button>
+
+      <h1 className="header">About Aline</h1>
+        <p className="p1">
           Welcome to Aline - Your One-Stop Solution for local events whether you're taking it solo or with friends!
         </p> 
 
         <h2>Welcome to Aline!</h2> 
-        <p>Discover the pulse of your local scene with Aline, your ultimate guide to finding and exploring exciting events around you.
+        <p className="p1">Discover the pulse of your local scene with Aline, your ultimate guide to finding and exploring exciting events around you.
           Whether it's a community festival, a vibrant market, or an exclusive concert,
           Aline makes it easy to uncover what's happening and stay in the know about event details and prices.
           Created by a dedicated group of collaborators,
@@ -38,39 +40,39 @@ const AboutAline = () => {
             Aline is here to support you every step of the way. We believe in innovation, integrity, and collaboration,
             and strive to embody these principles in everything we do.</p> 
 
-            <div className="team-container">
+<div className="team-container">
   <h2 className='h2'>Meet Our Team</h2>
+    <div className="team-member">
+      <h3><a href='https://github.com/TheReal4m4d3u5'>Avery Jacobson</a></h3>
+      <p>Front-end and Back-end Engineer</p>
+    </div>
+
   <div className="team-member">
-    <h3>Avery Jacobson</h3>
+    <h3><a href='https://github.com/cgroth06'>Chris Groth</a></h3>
     <p>Front-end and Back-end Engineer</p>
   </div>
 
   <div className="team-member">
-    <h3>Chris Groth</h3>
+    <h3><a href='https://github.com/ChristopherP-C'>Chris Persaud-cox</a></h3>
     <p>Front-end and Back-end Engineer</p>
   </div>
 
   <div className="team-member">
-    <h3>Chris Persaud-cox</h3>
-    <p>Front-end and Back-end Engineer</p>
-  </div>
-
-  <div className="team-member">
-    <h3>Dimintri Lo</h3>
+    <h3><a href='https://github.com/DimintriLo'>Dimintri Lo</a></h3>
     <p>Front-end Engineer</p>
   </div>
 
   <div className="team-member">
-    <h3>Josh Askew</h3>
+    <h3><a href='https://github.com/JoshAskew'>Josh Askew</a></h3>
     <p>Front-end and Back-end Engineer</p>
   </div>
-
 </div>
 
-        <h2>Get in Touch</h2>
-         <p>We are here to assist you with any questions or support you might need. Feel free to <a href="https://github.com/JoshAskew/Aline-Events">contact us</a> or
-        follow us on (social media links) to stay updated with the latest from Aline. Together, let's achieve greatness!</p>
-      </section>
+<h2>Get in Touch</h2>
+    <p className="p1">We're here to provide any <a className='contact' href=''>assistance or support</a> you may need.
+      Feel free to get in touch with us directly for the latest updates and information about Aline.
+      Together, let's achieve greatness!</p>
+</section>
   );
 };
   
