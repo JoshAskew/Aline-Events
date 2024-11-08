f4accf2aeb8900f8ef1ecbccd1423cbbd70ac759
JoshAskew <asksprit3@gmail.com>
2024-11-08 13:00:44 -0600

    card component changes

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,10 @@
 import React, { useEffect } from 'react';
 // import { Event } from '../interfaces/Events';
 // import { searchTicketMaster } from '../api/API';
-//import EventCard from '../components/EventCard';
+import EventCard from '../components/EventCard';
 
 import { Button, Card, Image, Text } from "@chakra-ui/react"
 import './Home.css'
-import Aline from "../images/aline.webp"
 import { Link } from "react-router-dom";
 import WeatherSidebar from "../components/SideBar";
 import AuthService from "../utils/auth";
@@ -146,80 +145,16 @@ const Home: React.FC = () => {
 
             <div className="cards-container">
                 
+                <EventCard />
+                <EventCard />
+                <EventCard />
+                <EventCard />
+                <EventCard />
+                <EventCard />
                 
-                <Card.Root className="card" maxW="sm" overflow="hidden">
-                    <Image
-                        src={Aline}
-                    />
-                    <Card.Body gap="2">
-                        <Card.Title>Event Title</Card.Title>
-                        <Card.Description>
-                            This is the type of the event.
-                        </Card.Description>
-                        <Card.Description>
-                            Start Date-End Date
-                        </Card.Description>
-                        <Text textStyle="2xl" fontWeight="medium" letterSpacing="tight" mt="2">
-                            $450-$1000
-                        </Text>
-                    </Card.Body>
-                    <Card.Footer gap="2">
-                        <Button variant="solid">Save Event</Button>
-                        <Button variant="ghost">Skip Event</Button>
-                    </Card.Footer>
-                </Card.Root>
             </div>
         </>
     );
-
-    // Saying the same thing 
-
-    // return (
-    //     <>
-    //         <WeatherSidebar />
-    //         <Link to="/SavedEvents">
-    //             <Button className="saved-button" size="sm" variant="outline">Saved Events</Button>
-    //         </Link>
-    //         <PopoverRoot>
-    //             <PopoverTrigger asChild>
-    //                 <Button className="logout" size="sm" variant="outline">
-    //                     Logout
-    //                 </Button>
-    //             </PopoverTrigger>
-    //             <PopoverContent>
-    //                 <PopoverArrow />
-    //                 <PopoverBody>
-    //                     <PopoverTitle fontWeight="bold">Are you sure you want to logout?</PopoverTitle>
-    //                     <Text my="4">
-    //                         These prices are not guaranteed to persist.
-    //                     </Text>
-    //                     <Link to="../Login">
-    //                         <Button className="logout" size="sm" variant="outline">
-    //                             Yes, Log Me Out
-    //                         </Button>
-    //                     </Link>
-    //                 </PopoverBody>
-    //             </PopoverContent>
-    //         </PopoverRoot>
-    //         <h1 className="header">Aline</h1>
-    //         <div className="cards-container">
-    //             <div>
-    //                 <div className="event-list">
-    //                     {(events.length > 0 && events.length - 1 !== eventsIndex) ? (
-    //                         <EventCard
-    //                             event={events[eventsIndex]}
-    //                             addToEventList={addToEventList}
-    //                             removeFromEvents={removeFromEvents}
-    //                         />
-    //                     ) : (
-    //                         <p>No Events found.</p>
-    //                     )}
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     </>
-    // );
-
 
 };
 
