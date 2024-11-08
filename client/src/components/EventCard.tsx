f0dc60e3d1ae4004791734ecddc438a547ca2fce
JoshAskew <asksprit3@gmail.com>
2024-11-08 13:00:44 -0600

    card component changes

--- client/src/components/EventCard.tsx
+++ client/src/components/EventCard.tsx
@@ -1,31 +1,32 @@
-import React from 'react';
+//import React from 'react';
 import { Card, Image, Text, Button } from "@chakra-ui/react";
-import { Event } from '../interfaces/Events.tsx';
+//import { Event } from '../interfaces/Events.tsx';
+import Aline from "../images/aline.webp"
 
-interface EventCardProps {
-  event: Event;
-  addToEventList: () => void | undefined;
-  removeFromEvents: () => void | undefined;
-}
-
-const EventCard: React.FC<EventCardProps> = ({ event, addToEventList, removeFromEvents }) => {
+    const EventCard = () =>  {
+      
   return (
     <Card.Root className="card" maxW="sm" overflow="hidden">
-      <Image src={event.imageUrl} alt={event.title} />
-      <Card.Body gap="2">
-        <Card.Title>{event.title}</Card.Title>
-        <Card.Description>{event.description}</Card.Description>
-        <Card.Description>{event.date}</Card.Description>
-        <Card.Description>{event.location}</Card.Description>
+    <Image
+        src={Aline}
+    />
+    <Card.Body gap="2">
+        <Card.Title>Event Title</Card.Title>
+        <Card.Description>
+            This is the type of the event.
+        </Card.Description>
+        <Card.Description>
+            Start Date-End Date
+        </Card.Description>
         <Text textStyle="2xl" fontWeight="medium" letterSpacing="tight" mt="2">
-          {event.price}
+            $450-$1000
         </Text>
-      </Card.Body>
-      <Card.Footer gap="2">
-        <Button variant="solid" onClick={addToEventList}>Save Event</Button>
-        <Button variant="ghost" onClick={removeFromEvents}>Skip Event</Button>
-      </Card.Footer>
-    </Card.Root>
+    </Card.Body>
+    <Card.Footer gap="2">
+        <Button variant="solid">Save Event</Button>
+        <Button variant="ghost">Skip Event</Button>
+    </Card.Footer>
+</Card.Root>
   );
 };
 
