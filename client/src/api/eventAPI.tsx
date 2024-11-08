d71e724f3a3b8ef0d9a7062523563b31ba2b1394
ChristopherP-C <christopherp.c.2day@gmail.com>
2024-11-07 20:03:53 -0600

    added eventAPI to client

---
+++ client/src/api/eventAPI.tsx
@@ -0,0 +1,73 @@
+import { Event } from '../interfaces/Events';
+import { ApiMessage } from '../interfaces/ApiMessage';
+import Auth from '../utils/auth';
+
+//get events saved to user profile
+export const getSavedEvents = async (userId: number): Promise<Event[]> => {
+    try {
+        const response = await fetch(`/api/events/saved/${userId}`, 
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${Auth.getToken()}`
+                }
+            }
+        );
+        const data = await response.json();
+
+        if (!response.ok) {
+            throw new Error('Invalid API response');
+        }
+        return data;
+    } catch (error) {
+        console.log(error);
+        return [];
+};
+};
+
+export const saveEvent = async (body: Event) => {
+    try {
+        const response = await fetch(`/api/events/save`, 
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${Auth.getToken()}`
+                },
+                body: JSON.stringify(body)
+            }
+        );
+        const data = await response.json();
+
+        if (!response.ok) {
+            throw new Error('Invalid API response');
+        }
+        return data;
+    } catch (error) {
+        console.log('Error saving event:', error);
+        return Promise.reject({ message: 'Error saving event' });
+    }
+};
+
+export const deleteEvent = async (id: number): Promise<ApiMessage> => {
+    try {
+        const response = await fetch(`/api/events/delete/${id}`, 
+            {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${Auth.getToken()}`
+                }
+            }
+        );
+        const data = await response.json();
+
+        if (!response.ok) {
+            throw new Error('Invalid API response');
+        }
+        return data;
+    } catch (error) {
+        console.log('Error deleting event:', error);
+        return Promise.reject({ message: 'Error deleting event' });
+    }
+}
